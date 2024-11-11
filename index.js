63445206d266c88627c706476835c0899049d0c4
Midhun vm <152033994+HEY-MIDHUU@users.noreply.github.com>
2024-11-11 19:00:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const chalk = require('chalk');
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const open = require('open');
 
+const app = express();
+const __path = process.cwd();
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
