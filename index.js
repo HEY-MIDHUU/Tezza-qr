d34b8fecbbc6fac262835a0adb8ec23cf02f7b96
Midhun vm <152033994+HEY-MIDHUU@users.noreply.github.com>
2024-11-11 18:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require("body-parser");
-const open = require('open');
+const  = require('express');
 
-const app = express();
-const __path = process.cwd();
-const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
